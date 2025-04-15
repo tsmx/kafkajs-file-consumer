0380803ded57b3bdd26e4992a1da1ace21d90551
tsmx <dev@tsmx.net>
2025-04-15 21:27:06 +0200

    Writing of output added, topic name adjusted.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { Kafka } from 'kafkajs';
+import fs from 'fs/promises';
+import path from 'path';
 
 const CLIENT_ID = 'kafkajs-file-consumer';
 const KAFKA_SERVER = 'localhost:9092';
 const CONSUMER_GROUP = 'kafkajs-group';
-const TOPIC = 'my.test.topic';
+const TOPIC = 'filetransfer';
 
 const kafka = new Kafka({
     clientId: CLIENT_ID,
@@ -20,6 +22,7 @@ await consumer.run({
     eachMessage: async ({ _topic, _partition, message, _heartbeat, _pause }) => {
         const fileBytes = Buffer.from(message.value);
         const fileName = message.headers['filename'].toString();
-        console.log({ time: new Date(), fileName: fileName, fileBytes });
+        await fs.writeFile(path.join('./output', fileName), fileBytes);
+        console.log(`File ${fileName} written. (${fileBytes.length} Bytes)`);
     }
 });
